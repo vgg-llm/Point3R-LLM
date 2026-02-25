3164d173c7dfe1b05c40638ce7c909ff34728655
Chanyoung Gwak <gwakcy@postech.ac.kr>
2026-02-25 18:20:41 +0900

    feat: demo_point3r change for arkitscene processing

--- scripts/demo/demo_point3r.py
+++ scripts/demo/demo_point3r.py
@@ -58,13 +58,13 @@ def load_models(load_point3r=True, device=None, model_path="Qwen/Qwen2.5-VL-3B-I
 
         # Load the base processor first
         print("Loading processor...")
-        # min_pixels = 256 * 32 * 32
-        # max_pixels = 256 * 32 * 32
+        min_pixels = 192 * 32 * 32
+        max_pixels = 600 * 32 * 32
         # max_pixels = 1280 * 32 * 32
         # min_pixels = 256 * 28 * 28
         # max_pixels = 1280 * 28 * 28
-        min_pixels = 256 * 28 * 28
-        max_pixels = 1280 * 28 * 28
+        # min_pixels = 256 * 28 * 28
+        # max_pixels = 1280 * 28 * 28
         base_processor = AutoProcessor.from_pretrained(
             model_path, use_fast=True, min_pixels=min_pixels, max_pixels=max_pixels
         )
@@ -274,7 +274,7 @@ def preprocess_images(
 
     assert (grid_thw == grid_thw[0]).all(), "Not all grid_thw entries are identical"
     t, h, w = grid_thw[0].tolist()
-    assert h <= w, "width cannot be smaller for Point3R"
+    # assert h <= w, "width cannot be smaller for Point3R"
     expected_width = (w // processor.image_processor.merge_size) * processor.image_processor.patch_size
     expected_height = (h // processor.image_processor.merge_size) * processor.image_processor.patch_size
 
